12e2684a951b0092a8a99a6428faf82212ff3f42
Ruben Pingol <128448242+rubenberttpingol@users.noreply.github.com>
2026-04-02 20:04:08 +0800

    Chore: Add periodic and XMR data update of data widget (#80)
    * Chore: Add periodic and XMR data update of data widget
    relates to xibosignage/electron-player#76
    
    * Update player version from config

--- src/main/config/config.ts
+++ src/main/config/config.ts
@@ -31,8 +31,8 @@ export class Config {
   readonly appType: string = 'electron';
 
   // App information
-  readonly version: string = "v4 R401";
-  readonly versionCode: number = 401;
+  readonly version: string = "v4 R403";
+  readonly versionCode: number = 403;
 
   // Config file
   readonly savePath: string;
